34e555d705ca4ad41afb9a5edb52445a3796bdc3
SanecZ93 <zyryanovalex93@gmail.com>
2022-08-06 11:26:32 +0300

    4.2. Результат работы метода [#232622]

---
+++ src/main/java/ru/job4j/MathFunc.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class MathFunc {
+    public static void func1(int x) {
+        int result3 = x;
+        System.out.println(result3);
+    }
+
+    public static void main(String[] args) {
+       MathFunc.func1(100);
+
+    }
+}
