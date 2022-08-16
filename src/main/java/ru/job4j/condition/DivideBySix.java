30d0ebfb4dec9abfb0c04bb42bf009ed072c5d20
SanecZ93 <zyryanovalex93@gmail.com>
2022-08-16 11:39:45 +0300

    111111

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -4,13 +4,13 @@ public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
         if (number % 3 == 0 && number % 2 == 0) {
-            rsl = "The original number is divided by 6.";
+            rsl = "Исходное число делится на 6.";
         } else if (number % 3 == 0) {
-            rsl = "The original number is divisible by 3, but is not even.";
+            rsl = "Исходное число делится на 3, но не является четным.";
         } else if (number % 2 == 0) {
-            rsl = "The original number is not divisible by 3, but is even.";
+            rsl = "Исходное число не делится на 3, но является четным.";
         } else {
-            rsl = "The original number is not divisible by 3 and is not even.";
+            rsl = "Исходное число не делится на 3 и не является четным.";
         }
         return rsl;
     }
