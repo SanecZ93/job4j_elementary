8a30de60eeb8d93ce63d6a5c470908b3ccfc4fbc
SanecZ93 <zyryanovalex93@gmail.com>
2022-08-19 10:46:21 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new  String[100500];
         float[] prices = new float[40];
+        System.out.println("The size of the array ages is: " + ages.length);
+        System.out.println("The size of the array surnames is: " + surnames.length);
+        System.out.println("The size of the array prices is: " + prices.length);
     }
 }
