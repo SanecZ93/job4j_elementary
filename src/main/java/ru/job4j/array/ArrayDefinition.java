a97cc9c91ee51789f07ee9407658893eb82cbd3e
SanecZ93 <zyryanovalex93@gmail.com>
2022-08-19 10:57:21 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new  String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Alex";
+        names[1] = "Fedor";
+        names[2] = "Pavel";
+        names[3] = "Marry";
         System.out.println("The size of the array ages is: " + ages.length);
         System.out.println("The size of the array surnames is: " + surnames.length);
         System.out.println("The size of the array prices is: " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
