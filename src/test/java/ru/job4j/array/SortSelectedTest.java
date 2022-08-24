a5dc729d90ed8d46ba9cf2f71566f6ab37e34ff6
SanecZ93 <zyryanovalex93@gmail.com>
2022-08-24 17:34:31 +0300

    6.5.3. Сортировка выборкой. [#156320 #335790]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -6,9 +6,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SortSelectedTest {
     @Test
     public void whenSort() {
-        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] data = new int[]{3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(data);
-        int[] expected = new int[] {1, 2, 3, 4, 5};
+        int[] expected = new int[]{1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort1() {
+        int[] data = new int[]{6, 8, 7};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{6, 7, 8};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort2() {
+        int[] data = new int[]{5, 1, 4, 3, 2};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] data = new int[]{5, 10, 4, 8, 2, 1, 6, 9, 7, 3};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertThat(result).containsExactly(expected);
     }
 }
