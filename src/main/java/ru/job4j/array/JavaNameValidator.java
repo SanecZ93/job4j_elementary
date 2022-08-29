7a169b8f9d30ac72431181e4dc9f7cfe99b3c553
SanecZ93 <zyryanovalex93@gmail.com>
2022-08-29 14:14:20 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -1,35 +1,34 @@
 package ru.job4j.array;
 
+import static com.sun.org.apache.xerces.internal.impl.dv.xs.TypeValidator.isDigit;
+import static java.lang.Character.isUpperCase;
+import static sun.util.locale.LocaleUtils.isEmpty;
+
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-
-        for (int i = 0; i < name.length(); i++) {
-         if (isSpecialSymbol(i) || isLowerLatinLetter(i)
-         || isUpperLatinLetter(i));
-
+        char[] array = name.toCharArray();
+        boolean valid = true;
+        if (!isEmpty(name) && !isDigit(array[0]) && !isUpperCase(array[0])) {
+            valid = false;
         }
-        return false;
+        for (int i = 0; i < array.length; i++) {
+            if (isSpecialSymbol(array[i]) || isLowerLatinLetter(array[i]) || isUpperLatinLetter(array[i])) {
+                valid = false;
+                break;
+            }
+        }
+        return valid;
     }
 
     public static boolean isSpecialSymbol(int code) {
-        if (code == 36 || code == 95) {
-
-        }
-            return false;
+        return false;
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        if (code >= 65 || code <= 90) {
-
-        }
         return false;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        if (code >= 97 || code <= 122) {
-
-        }
         return false;
     }
 }
-
